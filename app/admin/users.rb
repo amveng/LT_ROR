93f3930799d826d1ece14148fdd2202693a1d54d
Andrey Misyura <magway2k@mail.ru>
2020-05-30 13:00:30 +0300

    raznoe

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register User do
-  menu label: 'Пользователи'
+  # menu label: 'Пользователи'
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
