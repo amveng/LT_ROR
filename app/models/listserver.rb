ea9a53ba5f8e16fd1ae8507626b8970e8993c7dc
Andrey Misyura <magway2k@mail.ru>
2020-06-14 19:30:49 +0300

    add title.capitalize for server

--- app/models/listserver.rb
+++ app/models/listserver.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
 
+AutoStripAttributes::Config.setup do
+  set_filter(force_capitalize: false) do |value|
+    !value.blank? && value.respond_to?(:capitalize) && value.capitalize
+  end
+end
+
 class Listserver < ApplicationRecord
   belongs_to :user
 
-  auto_strip_attributes :title, squish: true
+  auto_strip_attributes :title, squish: true, force_capitalize: true
 
   validates :dateStart, :version, presence: true
   validates :title, uniqueness: true, length: { in: 4..42 }
   validates :urlServer, format: { with: /https/, message: 'Должен начинатся с "https"' }
 
+ 
   # scope :unpublish, lambda {
   #   where(publish: false)
   # }
+
+
 end
