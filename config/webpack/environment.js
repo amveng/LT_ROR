094d43a956465f2873c546f3b2214f98f63b2326
Andrey Misyura <magway2k@mail.ru>
2020-06-19 09:03:30 +0300

    datepicker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,5 +8,20 @@ const { environment } = require('@rails/webpacker')
 //     Popper: ['popper.js', 'default']
 //   })
 // )
+// const webpack = require('webpack')
+// environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+//   $: 'jquery/src/jquery',
+//   jQuery: 'jquery/src/jquery',
+//   jquery: 'jquery',
+//   'window.jQuery': 'jquery',
+//   Popper: ['popper.js', 'default']
+// }))
+// const webpack = require('webpack')
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery'
+//   })
+// )
 
 module.exports = environment
