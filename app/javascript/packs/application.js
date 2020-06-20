ec843af0a0cbff8389d6e69e1248d178d0c3fcab
Andrey Misyura <magway2k@mail.ru>
2020-06-20 03:43:30 +0300

    revision datepiker for i18n

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,16 +19,24 @@ require("channels")
 import 'bootstrap'
 
 import 'bootstrap-datepicker'
-
 // import 'jquery'
 
 // import * as $ from 'jquery';
 
 $(document).on("turbolinks:load", function(){
-    $('.datepicker').datepicker({
-        language: "ru",
+    $.fn.datepicker.dates['en'] = {
+        days: ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"],
+        daysShort: ["Вск", "Пнд", "Втр", "Срд", "Чтв", "Птн", "Суб"],
+        daysMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+        months: ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"],
+        monthsShort: ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+        today: "Сегодня",
+        clear: "Очистить",
         format: "yyyy-mm-dd",
-        weekStart: 1,
+        titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+        weekStart: 1
+    };
+    $('.datepicker').datepicker({              
         todayBtn: "linked",        
         todayHighlight: true,       
         autoclose: true,
