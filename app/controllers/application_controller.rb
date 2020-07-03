960d210ec7bdd4c8871d7916a3a646b0c931da3a
Andrey Misyura <magway2k@mail.ru>
2020-07-03 21:14:49 +0300

    test3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # protect_from_forgery prepend: true
+  protect_from_forgery prepend: true, with: :exception
   add_flash_types :success, :danger, :info, :warning
   # before_action :set_user_servers
 
-  rescue_from ActiveRecord::RecordNotFound do |_exception|
-    redirect_to root_path, alert: 'Record not found'
-  end
+  # rescue_from ActiveRecord::RecordNotFound do |_exception|
+  #   redirect_to root_path, alert: 'Record not found'
+  # end
 end
