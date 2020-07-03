fe09b32c93dd266d584920ff1efe79486bb4dedf
Andrey Misyura <magway2k@mail.ru>
2020-07-03 21:40:29 +0300

    test6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,12 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'lineagetop.com', from: 'admin@lineagetop.com' }
+  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener
+  config.app_domain = 'lineagetop.com' 
+  config.action_mailer.smtp_settings = Rails.application.credentials.mail
 end
