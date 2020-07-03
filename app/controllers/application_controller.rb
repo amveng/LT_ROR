40522a0052568fd6733a1061049d5dd24d42e790
Andrey Misyura <magway2k@mail.ru>
2020-07-03 21:24:33 +0300

    test5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   # protect_from_forgery prepend: true, with: :exception
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
   add_flash_types :success, :danger, :info, :warning
   # before_action :set_user_servers
 
