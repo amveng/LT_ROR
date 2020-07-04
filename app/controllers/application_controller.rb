38bfb516d8d1e21fa42206e7b6d153b0e9a31dc7
Andrey Misyura <magway2k@mail.ru>
2020-07-04 09:01:15 +0300

    test7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true, with: :exception
+  # protect_from_forgery prepend: true, with: :exception
   # protect_from_forgery with: :null_session
   add_flash_types :success, :danger, :info, :warning
   # before_action :set_user_servers
