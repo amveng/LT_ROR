9c31c881984c0618dbce8aa22617aca4854dd8c6
Andrey Misyura <magway2k@mail.ru>
2020-07-04 22:45:21 +0300

    raznoe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'lineagetop.com', from: 'admin@lineagetop.com' }
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :file
   # config.action_mailer.delivery_method = :letter_opener
   config.app_domain = 'lineagetop.com' 
   config.action_mailer.smtp_settings = Rails.application.credentials.mail_production
