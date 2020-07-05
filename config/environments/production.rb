42144172e82a7199dc614d04be7d2ea8f606ca97
Andrey Misyura <magway2k@mail.ru>
2020-07-05 21:17:45 +0300

    raznoe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "LT_ROR_production"
 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
