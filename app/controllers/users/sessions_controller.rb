f1dea791317f84a88278a4654211cea2d80a693f
Andrey Misyura <magway2k@mail.ru>
2020-07-09 03:07:20 +0300

    add recaptcha

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,19 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def create
+    recaptcha_valid = verify_recaptcha(action: 'session', minimum_score: 0.8)
+    if recaptcha_valid
+      super
+    else
+      build_resource(sign_up_params)
+      clean_up_passwords(resource)
+      flash.now[:alert] = "К сожалению гугл считает что вы бот. Пожалуйста, попробуйте еще раз."
+      flash.delete :recaptcha_error
+      render :new
+    end
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
