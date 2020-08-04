7e4e40f400d6a8c2c0b904a089e38dfe553ddfbc
Andrey Misyura <magway2k@mail.ru>
2020-08-04 05:58:55 +0300

    vote async

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,27 +11,9 @@ class VotesController < ApplicationController
     if current_user.votetime < DateTime.now
       current_user.votetime = DateTime.now + 12.hours
       # + 12.hours
-      # -------
-      f = @vote.server_id
-      server = Server.find(f)
-      time_range = DateTime.now.yesterday..DateTime.now
-      date_range = 7.days.ago..0.days.ago
-      votes_weekly_count = Vote.where(server_id: f, date: date_range).count
-      votes_days_count = Vote.where(server_id: f, created_at: time_range).count
-      votes_weekly_average = votes_weekly_count.to_f / 7
-      long_day = ((server.created_at - DateTime.now) / (60 * 60 * 24 * 356)).abs
-      votes_weekly_average = 1000 if votes_weekly_average > 1000
-      long_day = 0.99 if long_day > 0.99
-      rating = 4 - server.status + long_day + votes_weekly_average / 500
-      rating += 1 if votes_weekly_average > 10
-      rating += 1 if votes_weekly_count > 100
-      rating += 1 if votes_days_count > 1000
-      rating += 1 if votes_days_count > 3000
-      rating = rating.round(2)
-      server.update_attributes(rating: rating)
-      # ----------------
+      VoteWorker.perform_async(params[:id])
       if @vote.save && current_user.save
-        redirect_to server_path(server), success: 'Вы проголосовали'
+        redirect_to server_path(params[:id]), success: 'Вы проголосовали'
       else
         voting_failed
       end
