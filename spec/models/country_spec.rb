6e01df247b56f978a91e91649a9fda483ba7d2af
Andrey Misyura <magway2k@mail.ru>
2020-08-29 08:36:08 +0300

    tests for country model

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,48 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'валидации' do
+    it 'проверка на наличие названия страны' do
+      country = Country.new(code: 'code', iso3: 'iso', numeric: 1)
+      expect(country.save).to eq(false)
+    end
+
+    it 'проверка на наличие буквенного кода страны' do
+      country = Country.new(name: 'name', iso3: 'iso', numeric: 1)
+      expect(country.save).to eq(false)
+    end
+
+    it 'проверка на наличие кода iso3' do
+      country = Country.new(code: 'code', name: 'name', numeric: 1)
+      expect(country.save).to eq(false)
+    end
+
+    it 'проверка на наличие цифрового кода' do
+      country = Country.new(code: 'code', name: 'name', iso3: 'iso')
+      expect(country.save).to eq(false)
+    end
+
+    it 'проверка на максимальную длину названия страны' do
+      country = Country.new(name: ('A' * 101), code: 'code', iso3: 'iso', numeric: 1)
+      expect(country.save).to eq(false)
+    end
+
+    it 'проверка на максимальную длину буквенного кода' do
+      country = Country.new(name: 'name', code: '123456', iso3: 'iso', numeric: 1)
+      expect(country.save).to eq(false)
+    end
+
+    it 'проверка на максимальную длину кода iso3' do
+      country = Country.new(name: 'name', code: 'code', iso3: '1234', numeric: 1)
+      expect(country.save).to eq(false)
+    end
+
+    it 'проверка на уникальность кода страны' do
+      Country.create(name: 'name', code: 'code', iso3: '123', numeric: 2)
+      country = Country.new(name: 'name2', code: 'code', iso3: 'iso', numeric: 1)
+      expect(country.save).to eq(false)
+    end
+  end
 end
