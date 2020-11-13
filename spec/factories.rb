cbbd8041a65c4528c22b58dd3f16d8d27a6d6c9d
Andrey Misyura <magway2k@mail.ru>
2020-11-13 22:52:50 +0300

    tests for calculator services

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :user do
+    username { 'username' }
+    sequence(:email) { |i| "user#{i}@example.com" }
+    password { 'password' }
+  end
+
+  factory :vote do
+    date { Date.today }
+    user { build(:user) }
+    server { build(:server) }
+  end
+
   factory :serverversion do
     name { 'version1' }
   end
